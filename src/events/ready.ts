7ed6ca3bfc84881df21666fddcfeb041c1fa515e
cupcakehy <cupcakehy@gmail.com>
2024-11-06 11:38:37 +0100

    Fixed config

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import config from 'config';
 import { ActivityType } from 'discord.js';
 import { event, Events } from '../utils/index.js';
 
-const channel = config.get('twitch.channel')
+const channel = config.get('twitch.broadcaster')
 
 export default event(Events.ClientReady, async ({ log }, client) => {
 	client.user.setPresence({ activities: [{ name: 'Caitlyn64', type: ActivityType.Streaming, url: `https://www.twitch.tv/${channel}` }] });
